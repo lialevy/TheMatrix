6c8d9615a12e7ac45a6e95db5831bde2664b90c4
Dor Shmaryahu <dshmaryahu@gmail.com>
2020-08-08 20:59:52 +0300

    Implemented game setup requirements

---
+++ src/app/classes/index.ts
@@ -0,0 +1,15 @@
+import Game from './Game.class';
+import TwoPlayerGame, { TwoPlayerMatrix } from './TwoPlayerGame.class';
+import Player from './Player.class';
+import Round from './Round.class';
+
+type Matrix = TwoPlayerMatrix;
+
+export {
+  Game,
+  TwoPlayerGame,
+  Matrix,
+  TwoPlayerMatrix,
+  Player,
+  Round,
+}
