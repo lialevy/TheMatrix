f3587fa663082af14a0a7fa7552abb5eedd7b902
Lia Levy <lia@lias-mbp.home>
2020-08-14 16:17:04 +0300

    present strategies in results table

--- src/app/views/result-page/result-page.component.ts
+++ src/app/views/result-page/result-page.component.ts
@@ -13,25 +13,37 @@ export class ResultPageComponent implements OnInit {
   gameResults: Results;
   displayedColumns: String[];
   dataSource: any;
+  isTwoPlayers: boolean;
   firstPlayer: Player;
   secondPlayer: Player;
   thirdPlayer: Player;
 
   ngOnInit(): void {
     this.gameResults = this.gameService.getGameResults();
+    this.isTwoPlayers = true;
     this.firstPlayer = this.gameResults.scoreTable[0];
     this.secondPlayer = this.gameResults.scoreTable[1];
+
     if (this.gameResults.scoreTable.length === 3) {
+      this.isTwoPlayers = false;
       this.thirdPlayer = this.gameResults.scoreTable[2];
     }
+    console.log(this.gameResults.roundsTable[0].playedStrategies);
 
-    this.displayedColumns = ["Round No.", "Result"];
+    this.displayedColumns = ["Round No.", "Result", "Strategies"];
     let i = 1;
     this.dataSource = this.gameResults.roundsTable.map((round) => {
       const tableRows = {
         roundNum: i++,
         result: round.result,
+        strategies: [
+          round.playedStrategies[0].strategy,
+          round.playedStrategies[1].strategy,
+        ],
       };
+      if (!this.isTwoPlayers) {
+        tableRows.strategies.push(round.playedStrategies[2].strategy);
+      }
 
       return tableRows;
     });
