1b062ab43270b718eb98eab99723e7165e2da98b
Lia Levy <lia@lias-mbp.home>
2020-08-22 15:19:37 +0300

    added summary table in result page, pure equilibrium

--- src/app/views/result-page/result-page.component.ts
+++ src/app/views/result-page/result-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { GameService } from "../../services/game.service";
-import { Round, Player } from "src/app/classes";
+import { Round, Player, MixedStrategy } from "src/app/classes";
 import { Results } from "src/app/services/game-service.interface";
 
 @Component({
@@ -14,24 +14,21 @@ export class ResultPageComponent implements OnInit {
   displayedColumns: string[];
   dataSource: any;
   isTwoPlayersGame: boolean;
-  // firstPlayer: Player & { place?: 1 | 2 | 3 };
-  // secondPlayer: Player & { place?: 1 | 2 | 3 };
-  // thirdPlayer: Player & { place?: 1 | 2 | 3 };
+
   scoreTable: (Player & { place?: 1 | 2 | 3 })[];
   firstPlacePlayer: (Player & { place?: 1 | 2 | 3 })[];
   secondPlacePlayer: (Player & { place?: 1 | 2 | 3 })[];
   thirdPlacePlayer: (Player & { place?: 1 | 2 | 3 })[];
 
+  mixedStrategySummary: MixedStrategy[];
+  summaryDisplayedColumns: string[];
+  summaryDataSource: any;
+
   ngOnInit(): void {
     this.gameResults = this.gameService.getGameResults();
     this.isTwoPlayersGame = this.gameService.getPlayers().length === 2;
 
-    // this.firstPlayer = this.gameResults.scoreTable[0];
-    // this.secondPlayer = this.gameResults.scoreTable[1];
-    // if (!this.isTwoPlayersGame) {
-    //   this.thirdPlayer = this.gameResults.scoreTable[2];
-    // }
-
+    //podium
     this.firstPlacePlayer = [];
     this.secondPlacePlayer = [];
     this.thirdPlacePlayer = [];
@@ -47,7 +44,13 @@ export class ResultPageComponent implements OnInit {
       }
     });
 
-    this.displayedColumns = ["Round No.", "Result", "Strategies"];
+    //score table
+    this.displayedColumns = [
+      "Round No.",
+      "Result",
+      "Strategies",
+      "Pure Equilibrium",
+    ];
     let i = 1;
     this.dataSource = this.gameResults.roundsTable.map((round) => {
       const tableRows = {
@@ -57,6 +60,7 @@ export class ResultPageComponent implements OnInit {
           round.playedStrategies[0].strategy,
           round.playedStrategies[1].strategy,
         ],
+        pureEquilibrium: round.pureEquilibrium,
       };
       if (!this.isTwoPlayersGame) {
         tableRows.strategies.push(round.playedStrategies[2].strategy);
@@ -64,5 +68,37 @@ export class ResultPageComponent implements OnInit {
 
       return tableRows;
     });
+
+    // summary table
+    this.mixedStrategySummary = this.gameResults.mixedStrategies;
+    this.summaryDisplayedColumns = [
+      "Player",
+      "Mixed Strategy",
+      "Expected Value",
+    ];
+
+    const playerMixedStrategyByNumber = this.gameResults.mixedStrategies.map(
+      (playerMixedStrategy) => {
+        return playerMixedStrategy.strategy.map((s) => {
+          return [`${s.strategy}: ${s.probability}`];
+        });
+      }
+    );
+
+    this.summaryDataSource = this.gameResults.mixedStrategies.map(
+      (playerMixedStrategy) => {
+        const summaryTableRows = {
+          player: playerMixedStrategy.player.name,
+          strategies: [
+            playerMixedStrategyByNumber[
+              playerMixedStrategy.player.playerNumber
+            ],
+          ],
+          expectedValue: playerMixedStrategy.expectedValue,
+        };
+
+        return summaryTableRows;
+      }
+    );
   }
 }
