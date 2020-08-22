8ec28bf3089415c2ffbf003474c2a4cea660bfbc
Dor Shmaryahu <dshmaryahu@gmail.com>
2020-08-22 12:37:21 +0300

    Refractored game service to use getters instead of functions

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import {
   Game,
   Matrix,
@@ -7,59 +7,76 @@ import {
   Round,
   ThreePlayerGame,
   TwoPlayerGame,
-} from "../classes";
-import { GameType } from "../classes/Game.class";
-import Templates from "../templates";
-import GameServiceInterface, { Results } from "./game-service.interface";
+} from '../classes';
+import { GameType } from '../classes/Game.class';
+import Templates from '../templates';
+import GameServiceInterface, { Results } from './game-service.interface';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class GameService implements GameServiceInterface {
-  private game: Game;
-  private numberOfPlayers: number;
-  #playerTemplatesSubject: BehaviorSubject<string[]> = new BehaviorSubject<
-    string[]
-  >([]);
+  #game: Game;
+  #numberOfPlayers: number;
+  #playerTemplatesSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
   #currentTemplates: { [propName: string]: any };
 
   gameFinished$: Observable<boolean>;
-  playerTemplates$: Observable<
-    string[]
-  > = this.#playerTemplatesSubject.asObservable();
+  playerTemplates$: Observable<string[]> = this.#playerTemplatesSubject.asObservable();
   lastRound$: Observable<Round>;
+  playerScores$: Observable<number[]>;
+  currentRoundNumber$: Observable<number>;
+
+  get numberOfRounds(): number {
+    return this.#game.numberOfRounds;
+  }
+
+  get players(): Player[] {
+    return this.#game.players;
+  }
+
+  get matrix(): Matrix {
+    return this.#game.matrix;
+  }
+
+  get playerStrategies(): string[][] {
+    return this.#game.matrix.playersStrategies;
+  }
 
   constructor() {
     this.setNumberOfPlayers(2);
   }
 
   setNumberOfPlayers(numberOfPlayers: number): void {
-    this.numberOfPlayers = numberOfPlayers;
+    this.#numberOfPlayers = numberOfPlayers;
 
-    if (this.numberOfPlayers === 2) {
-      this.game = new TwoPlayerGame();
+    if (this.#numberOfPlayers === 2) {
+      this.#game = new TwoPlayerGame();
       this.#currentTemplates = Templates.TwoPlayerTemplates;
-    } else if (this.numberOfPlayers === 3) {
-      this.game = new ThreePlayerGame();
+    } else if (this.#numberOfPlayers === 3) {
+      this.#game = new ThreePlayerGame();
       this.#currentTemplates = Templates.ThreePlayerTemplates;
     } else {
-      throw new Error("numberOfPlayers must be 2 or 3");
+      throw new Error('numberOfPlayers must be 2 or 3');
     }
 
-    console.log(Templates.TwoPlayerTemplates);
-
-
     this.#playerTemplatesSubject.next(Object.keys(this.#currentTemplates));
-    this.lastRound$ = this.game.lastRound$;
-    this.gameFinished$ = this.game.gameFinished$;
+    this.lastRound$ = this.#game.lastRound$;
+    this.gameFinished$ = this.#game.gameFinished$;
+    this.playerScores$ = this.#game.playerScores$;
+    this.currentRoundNumber$ = this.#game.currentRound$;
   }
 
+  // DEPRECATED
+  /**
+   * @deprecated use numberOfRounds instead
+   */
   getNumberOfRounds(): number {
-    return this.game.numberOfRounds;
+    return this.#game.numberOfRounds;
   }
 
   setNumberOfRounds(numberOfRounds: number): void {
-    this.game.setNumberOfRounds(numberOfRounds);
+    this.#game.setNumberOfRounds(numberOfRounds);
   }
 
   createGameMatrixByDimensions(
@@ -67,7 +84,7 @@ export class GameService implements GameServiceInterface {
     columns: number,
     depth?: number
   ): Matrix {
-    return this.game.createGameMatrix(rows, columns, depth);
+    return this.#game.createGameMatrix(rows, columns, depth);
   }
 
   createGameMatrixByTemplate(templateName: string): Matrix {
@@ -76,7 +93,7 @@ export class GameService implements GameServiceInterface {
     if (!template) {
       throw new Error(`Template ${templateName} doesn't exist`);
     } else {
-      return this.game.createGameMatrixByTemplate(template);
+      return this.#game.createGameMatrixByTemplate(template);
     }
   }
 
@@ -85,46 +102,66 @@ export class GameService implements GameServiceInterface {
     minValue = 0,
     maxValue = 10
   ): Matrix {
-    return this.game.generateRandomMatrixValues(type, minValue, maxValue);
+    return this.#game.generateRandomMatrixValues(type, minValue, maxValue);
   }
 
   setMatrixValues(matrix: Matrix): void {
-    this.game.matrix = matrix;
+    this.#game.matrix = matrix;
   }
 
   /**
    * Validate game is ready and finalize game creation if required
    */
   finalizeGameSetup(): [boolean, string[]] {
-    this.game.finalizeGameSetup();
-    return this.game.validateGame();
+    this.#game.finalizeGameSetup();
+    return this.#game.validateGame();
   }
 
+  // DEPRECATED
+  /**
+   * @deprecated use players instead
+   */
   getPlayers(): Player[] {
-    return this.game.players;
+    return this.#game.players;
   }
 
+  // DEPRECATED
+  /**
+   * @deprecated use matrix instead
+   */
   getGameMatrix(): Matrix {
-    return this.game.matrix;
+    return this.#game.matrix;
   }
 
+  // DEPRECATED
+  /**
+   * @deprecated use playerScores$ instead
+   */
   getPlayerScoresObservable(): Observable<number[]> {
-    return this.game.playerScores$;
+    return this.#game.playerScores$;
   }
 
+  // DEPRECATED
+  /**
+   * @deprecated use currentRoundNumber$ instead
+   */
   getRoundNumberObservable(): Observable<number> {
-    return this.game.currentRound$;
+    return this.#game.currentRound$;
   }
 
+  // DEPRECATED
+  /**
+   * @deprecated use playerStrategies instead
+   */
   getPlayerStrategies(): string[][] {
-    return this.game.matrix.playersStrategies;
+    return this.#game.matrix.playersStrategies;
   }
 
   submitPlayerStrategy(player: number, strategy: string): void {
-    this.game.submitPlayerStrategy(player, strategy);
+    this.#game.submitPlayerStrategy(player, strategy);
   }
 
   getGameResults(): Results {
-    return this.game.getGameResults();
+    return this.#game.getGameResults();
   }
 }
